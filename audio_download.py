c1002c840f297baa7aaeac1ef3782b70b6df3e90
welcozh <sduzwk@163.com>
2020-08-22 20:02:17 +0800

    fix: change the structure

--- audio_download.py
+++ audio_download.py
@@ -5,7 +5,7 @@ import requests
 import jsonpath
 
 
-def song_load(url, title):
+def song_load(listbox, url, title):
     os.makedirs("音乐下载", exist_ok=True)
     path = "音乐下载\\{}.mp3".format(title)
     listbox.insert(tk.END, '歌曲: {}，正在下载...'.format(title))
@@ -19,7 +19,7 @@ def song_load(url, title):
     listbox.update()
 
 
-def get_music_name():
+def get_music_name(listbox, entry):
     name = entry.get()
     headers = {
         'X-Requested-With': 'XMLHttpRequest'
@@ -35,41 +35,45 @@ def get_music_name():
     resp = requests.post(url, data=params, headers=headers)
     data = resp.json()
     title = jsonpath.jsonpath(data, "$..title")[0]
-    author = jsonpath.jsonpath(data, "$..author")[1]
+    # author = jsonpath.jsonpath(data, "$..author")[0]
     url = jsonpath.jsonpath(data, "$..url")[0]
 
     # print(title)
     # print(author)
     # print(url)
-    song_load(url, title)
+    song_load(listbox, url, title)
 
 
 # get_music_name()
 
-mainWindow = tk.Tk()
+def display():
+    mainWindow = tk.Tk()
 
-mainWindow.title("音乐下载")
-mainWindow.geometry('560x450')
+    mainWindow.title("音乐下载")
+    mainWindow.geometry('560x450')
 
-# 标签
-label = tk.Label(mainWindow, text="请输入下载歌曲：", font=('宋体', 20))
-label.grid()
+    # 标签
+    label = tk.Label(mainWindow, text="请输入下载歌曲：", font=('宋体', 20))
+    label.grid()
 
-# 输入框
-entry = tk.Entry(mainWindow, font=('宋体', 20))
-entry.grid(row=0, column=1)
+    # 输入框
+    entry = tk.Entry(mainWindow, font=('宋体', 20))
+    entry.grid(row=0, column=1)
 
-# 列表框
-listbox = tk.Listbox(mainWindow, font=('', 15), width=55, heigh=16)
-listbox.grid(row=1, columnspan=2)
+    # 列表框
+    listbox = tk.Listbox(mainWindow, font=('', 15), width=55, heigh=16)
+    listbox.grid(row=1, columnspan=2)
 
-# 下载按钮
-button1 = tk.Button(mainWindow, text='下载', font=('宋体', 15), command=get_music_name)
-button1.grid(row=2, column=0, sticky=tk.W)
+    # 下载按钮
+    button1 = tk.Button(mainWindow, text='下载', font=('宋体', 15), command=lambda:get_music_name(listbox, entry))
+    button1.grid(row=2, column=0, sticky=tk.W)
 
-button2 = tk.Button(mainWindow, text='退出', font=('宋体', 15), command=mainWindow.quit)
-button2.grid(row=2, column=1, sticky=tk.E)
+    button2 = tk.Button(mainWindow, text='退出', font=('宋体', 15), command=mainWindow.quit)
+    button2.grid(row=2, column=1, sticky=tk.E)
 
-# 显示界面
-mainWindow.mainloop()
+    # 显示界面
+    mainWindow.mainloop()
+
+if __name__ == '__main__':
+    display()
 
